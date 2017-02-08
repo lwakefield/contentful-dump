9c78ad7b2d4a30d5826fbd6c5c35c3c962ae3b45
Lawrence Wakefield <lawrence@iamlawrence.me>
2017-02-08 11:34:16 -0500

    Tidies up fetch-assets

--- fetch-assets.js
+++ fetch-assets.js
@@ -1,38 +1,76 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony
+
 const request = require('request')
 const {execSync} = require('child_process')
 const {writeFileSync} = require('fs')
+const parser = new (require('argparse').ArgumentParser)
 
-execSync('mkdir -p dump')
+let args
+function init () {
+    parser.addArgument(
+        ['-o', '--output'],
+        {
+            defaultValue: 'dump',
+            help: 'Path to dump into'
+        }
+    )
+    parser.addArgument(
+        ['-v', '--verbose'],
+        {action: 'storeTrue'}
+    )
+    args = parser.parseArgs()
 
-let input = ''
-function run () {
+    execSync(`mkdir -p ${args.output}`)
+
+    return new Promise((res, rej) => {
+        let input = ''
+        process.stdin.on('readable', () => {
+            var chunk = process.stdin.read();
+            if (chunk !== null) input += chunk.toString()
+        })
+
+        process.stdin.on('end', () => res(input));
+    })
+}
+
+
+async function run (input) {
     const assets = JSON.parse(input).assets
-    const pendingRequests = []
+    const count = getAssetCount(assets)
+    debug(`Fetching ${count} assets`)
+
+    let fetched = 0
     for (const asset of assets) {
         const localizedFiles = asset.fields.file
         for (const locale in localizedFiles) {
             const file = localizedFiles[locale]
-            pendingRequests.push(
-                fetchAndDump(`https:${file.url}`, `dump/${locale}_${asset.sys.id}`)
-            )
+            const data = await fetch(`https:${file.url}`)
+            writeFileSync(`${args.output}/${locale}_${asset.sys.id}`, data)
+            debug(`Fetched ${fetched++} / ${count}`)
         }
     }
-    Promise.all(pendingRequests)
 }
 
-function fetchAndDump(url, path) {
+function getAssetCount(assets) {
+    let count = 0
+    for (const asset of assets) {
+        count += Object.keys(asset.fields.file || {}).length
+    }
+    return count
+}
+
+function fetch (url) {
     return new Promise((res, rej) => {
         request(url, (err, response, body) => {
-            writeFileSync(path, body)
-            res()
+            if (err) return rej(err)
+            res(body)
         })
     })
 }
 
-process.stdin.on('readable', () => {
-    var chunk = process.stdin.read();
-    if (chunk !== null) input += chunk.toString()
-})
+function debug(str) {
+    args.verbose && console.log(str)
+}
 
-process.stdin.on('end', run);
+init()
+    .then(input => run(input))
